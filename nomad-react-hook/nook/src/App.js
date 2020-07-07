1980dc890bcd4ee3e3cb9aff07934c9915989c5f
Yunho Choi <choiking10@kaist.ac.kr>
2020-07-07 14:51:00 +0900

    nomad-react-hook 1.1 remove unused code.

--- nomad-react-hook/nook/src/App.js
+++ nomad-react-hook/nook/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
@@ -14,32 +13,6 @@ function App() {
       <button onClick={decreaseCount}>Decrement</button>
     </div>
   );
-}
-
-class UglyApp extends React.Component {
-  state = {
-    count: 1,
-  };
-  increaseCount = () =>
-    this.setState((state) => {
-      return { count: state.count + 1 };
-    });
-  decreaseCount = () =>
-    this.setState((state) => {
-      return { count: state.count - 1 };
-    });
-
-  render() {
-    const { count } = this.state;
-
-    return (
-      <div className="App">
-        <h1>Hello Count: {count}</h1>
-        <button onClick={this.increaseCount}>Increment</button>
-        <button onClick={this.decreaseCount}>Decrement</button>
-      </div>
-    );
-  }
 }
 
 export default App;
